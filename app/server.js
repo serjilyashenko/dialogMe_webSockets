e9808b0d25a00cbf3239a705603065aed357ef6f
sky <sky.ilyashenko@gmail.com>
2016-08-19 11:20:47 +0300

    static server

--- app/server.js
+++ app/server.js
@@ -1,4 +1,6 @@
 var http = require('http'),
+    static = require('node-static'),
+    staticServer = new static.Server('../'),
     httpSever;
 
 httpServer = http.createServer(accept).listen(8080);
@@ -6,4 +8,6 @@ console.log('web server created on port 8080');
 
 function accept(request, response) {
     console.log(request.url);
+
+    staticServer.serve(request, response);
 }
