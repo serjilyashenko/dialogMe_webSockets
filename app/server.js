7db4bd17490c0cecad7ef8a85977be16a77fc89e
sky <sky.ilyashenko@gmail.com>
2016-08-19 14:23:08 +0300

    websocket communication

--- app/server.js
+++ app/server.js
@@ -1,9 +1,12 @@
 var http = require('http'),
     static = require('node-static'),
+    WebSocketServer = require('ws').Server,
     staticServer = new static.Server('../'),
-    httpSever;
+    httpSever,
+    webSocketServer;
 
 httpServer = http.createServer(accept).listen(8080);
+webSocketServer = new WebSocketServer({server: httpServer});
 console.log('web server created on port 8080');
 
 function accept(request, response) {
@@ -11,3 +14,11 @@ function accept(request, response) {
 
     staticServer.serve(request, response);
 }
+
+webSocketServer.on('connection', function(ws) {
+    ws.send('hello, client');
+
+    ws.on('message', function (message) {
+        console.log(message);
+    });
+});
--- js/Chat.js
+++ js/Chat.js
@@ -1,6 +1,11 @@
 var Chat = (function () {
-    var Chat = function () {
-        console.log('Chat constructor');
+    var Chat = function (url) {
+        var ws = new WebSocket(url);
+
+        ws.onmessage = function (event) {
+            console.log(event.data);
+            ws.send('hello, server');
+        };
     };
     
     return Chat;
